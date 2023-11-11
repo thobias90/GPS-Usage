72dccf1230d19f629588cc19179e87071ac31695
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-11-11 14:42:24 -0300

    refactor: add missing import

--- app/src/main/java/com/stahlt/gps_usage/MainActivity.kt
+++ app/src/main/java/com/stahlt/gps_usage/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
